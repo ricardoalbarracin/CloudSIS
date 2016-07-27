f864ace3b01bc71fdee1a9706ba37ead3730db1c
ricardoa88 <ricardoa88@gmail.com>
2016-07-26 22:36:49 -0500

    Primer Comit

---
+++ WebLayer/Scripts/nPe5n.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $('.navbar-toggle').click(function () {
+        console.log("navbar-toggle");
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+
+    });
+
+    // Remove menu for searching
+    $('#search-trigger').click(function () {
+        console.log("search-trigger");
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').removeClass('slide-in');
+
+    });
+});
